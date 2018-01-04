870805b64024746761d67332c2c37d3541d07479
lior.margalit <lior.margalit@hcl.com>
2018-01-04 18:22:23 +0200

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+
+const http = require('http');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const rippleURL= 'https://coinmarketcap.com/currencies/ripple/';
+
+async function getCoinsScreen() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        gpu: false,
+        scrollbars: false,
+        args: ['--reduce-security-for-testing', '--deterministic-fetch','--disable-background-networking' ]
+    });
+
+    const page = await browser.newPage();
+    await page.goto(rippleURL,{waitUntil: 'networkidle2'});
+
+    await page.screenshot({ path: 'ripple.jpg' });
+    browser.close();
+}
+getCoinsScreen();
+
+var server = http.createServer(function (req, res) {
+    if (req.url.search('ripple.jpg')>0)
+    {
+        displayripple(res);
+    }
+    else{
+        displayGrid(res);
+    }
+    
+});
+
+function displayGrid(res) {
+    fs.readFile('coins.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+function displayripple(res) {
+    fs.readFile('ripple.jpg', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'image',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+server.listen(8880);
+console.log("server listening on 8888");
